e8dd645d236db222030fb6d5457860f0aac9d04d
Jennifer Lê <jnnnle.sf@gmail.com>
2016-12-03 12:59:15 -0800

    Server port:2424 + psuedo plan

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,15 @@
 //setup server here
+const Express = require('express');
+const bodyParser = require('body-parser');
+const app = Express();
+
+//middleware 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//TODO: add routes for POST, PUT, DELETE
+
+//start with POST request to create a new todo list 
+
+
+app.listen(2424, (() => console.log('Hello world, server listening on port:2424')));
