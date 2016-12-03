eb415fd3c6e1b48a0f6131a4b4e0879bb5c23d7b
Jennifer Lê <jnnnle.sf@gmail.com>
2016-12-03 13:19:54 -0800

    HTTP verb + endpoint setup

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,30 @@
 //setup server here
 const Express = require('express');
-const bodyParser = require('body-parser');
+const BodyParser = require('body-parser');
 const app = Express();
+const data = require('./data');
 
 //middleware 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
 
 //TODO: add routes for POST, PUT, DELETE
 
-//start with POST request to create a new todo list 
+//start with POST request to create a new todo list
+app.post('/api/task', ((req, res) => {
+	//push the user input into data.tasks
+	res.send(data.tasks);
+}));
+
+//PUT
+app.put('/api/task', ((req, res) => {
+	//find task and update it's context
+}));
+
+//DELETE
+app.delete('/api/task', ((req, res) => {
+	//find task and delete it 
+}));
 
 
 app.listen(2424, (() => console.log('Hello world, server listening on port:2424')));
